b7f1d2a83a64e88d5c0be93c26283370e9665b5e
isarsilva <isasramos12@gmail.com>
2025-07-29 15:06:52 -0300

    criacao do disco, criacao tabuleiro

---
+++ main.js
@@ -0,0 +1,26 @@
+const eH1 = document.querySelector("h1");
+        eH1.textContent = "Ola mundo!"
+
+const eTabuleiro = criaTabuleiro();
+ document.body.append(eTabuleiro);
+
+for (let i=0; i<7; i++){
+    if (i==3) continue;
+     const disco1 = criaDisco(i<3? "branco":"preto",i);
+     eTabuleiro.append(disco1);
+}
+
+
+function criaDisco(cor, posicao){
+    const novoDisco = document.createElement("div");
+    novoDisco.classList.add("disco");
+    novoDisco.dataset.cor = cor;
+    novoDisco.dataset.posicao = posicao;
+    return novoDisco;
+}
+
+function criaTabuleiro(){
+    const eTabuleiro = document.createElement("div");
+    eTabuleiro.classList.add("tabuleiro");
+   return eTabuleiro;
+}
